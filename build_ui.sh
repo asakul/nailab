570e8d9030824708e18f92debc42f64829b4cf07
Denis Tereshkin <denis@kasan.ws>
2018-12-12 21:41:44 +0700

    Fix build_ui.sh, remove mainwindow.py.py
    Also, some minor tweaks in mainwindow

--- build_ui.sh
+++ build_ui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/pyuic5 ui/csvdatasourceconfigwidget.ui > src/nailab/ui_gen/csvdatasourceconfigwidget.py
-/usr/bin/pyuic5 ui/mainwindow.ui > src/nailab/ui_gen/mainwindow.py.py
+/usr/bin/pyuic5 ui/mainwindow.ui > src/nailab/ui_gen/mainwindow.py
 /usr/bin/pyuic5 ui/newdatasourcedialog.ui > src/nailab/ui_gen/newdatasourcedialog.py
 /usr/bin/pyuic5 ui/strategywidget.ui > src/nailab/ui_gen/strategywidget.py
 
